c268f981ed2e1f40b1ce013752045f168808809d
Cooper S <mcoopersandys@gmail.com>
2021-11-21 23:40:34 -0500

    eyo #4

--- src/City.java
+++
@@ -1,8 +0,0 @@
-public class City {
-    StreetMap streetMap;
-    LogSystem logSystem;
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Simulation.java
@@ -0,0 +1,7 @@
+public class Simulation {
+    public static void main(String[] args) {
+        StreetMap streetMap = new StreetMap();
+        LogSystem logSystem = new LogSystem();
+        System.out.println("Looking good!");
+    }
+}
