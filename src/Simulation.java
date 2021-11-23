bf571d3c7ce75f0ff91e8f696f68a4efb77080ce
Cooper S <mcoopersandys@gmail.com>
2021-11-23 17:32:28 -0500

    changed default value to instead scan for specified xml file

--- src/Simulation.java
+++ src/Simulation.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Simulation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        StreetMap streetMap = new StreetMap("testmap.xml", Integer.parseInt(args[0])); // TODO: StreetMap streetMap = new StreetMap(scan.nextLine());
+        StreetMap streetMap = new StreetMap(scan.nextLine(), Integer.parseInt(args[0])); // TODO: StreetMap streetMap = new StreetMap(scan.nextLine());
         LogSystem logSystem = new LogSystem();
         System.out.println("Looking good!");
         scan.close();
