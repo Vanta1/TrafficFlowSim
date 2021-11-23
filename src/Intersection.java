ac48f2daaf16f3c5d38f77833a48e3d5f2d81523
Cooper S <mcoopersandys@gmail.com>
2021-11-23 00:13:17 -0500

    added more file parsing

--- src/Intersection.java
+++ src/Intersection.java
@@ -1,3 +1,7 @@
 public class Intersection {
-    
+    private int id; // intersections will remain unnamed for now
+
+    public Intersection(int id) {
+        this.id = id;
+    }
 }
--- src/StreetMap.java
+++ src/StreetMap.java
@@ -1,3 +1,4 @@
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -5,14 +6,17 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
 import org.w3c.dom.Document;
 
 public class StreetMap {
     Car[] cars;
-    Street[] streets;
-    Intersection[] intersections;
+    ArrayList<Street> streets = new ArrayList<Street>();
+    ArrayList<Intersection> intersections = new ArrayList<Intersection>();
 
-    public StreetMap(String mapFile) {
+    public StreetMap(String mapFile, int numCars) {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         try {
             documentBuilderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
@@ -20,11 +24,22 @@ public class StreetMap {
             Document document = documentBuilder.parse(new File(mapFile));
             document.getDocumentElement().normalize();
 
-            // TODO: Streets
+            NodeList docIntersections = document.getElementsByTagName("intersec");
 
-            // TODO: Intersections
+            // TODO: constructing graph with streets
 
-        } catch (ParserConfigurationException | SAXException | IOException e) {
+            for (int i = 0; i < docIntersections.getLength(); i++) {
+                this.intersections.add(new Intersection(Integer.parseInt( // add an intersection for each intersection in xml file
+                        docIntersections.item(i)
+                                .getAttributes()
+                                .getNamedItem("id")
+                                .getNodeValue()
+                )));
+            }
+
+
+        } catch (ParserConfigurationException | SAXException |
+                IOException e) {
             e.printStackTrace();
         }
 
